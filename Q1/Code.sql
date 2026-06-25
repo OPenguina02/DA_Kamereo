6672f07d13d020cb7b9dbeda0429503bf267a96f
Hoang Quang Thang (Tony) <thanggas11@gmail.com>
2026-06-25 12:05:19 +0700

    Update Code.sql

--- Q1/Code.sql
+++ Q1/Code.sql
@@ -4,12 +4,10 @@ WITH f AS (SELECT channel, category, gmv
     WHERE gmv >= 1 AND NOT (category = 'Non-veggie' AND gmv > 70)),
 r AS (SELECT *,
            ROW_NUMBER() OVER(PARTITION BY channel ORDER BY gmv DESC) AS br,
-           ROW_NUMBER() OVER(PARTITION BY channel ORDER BY gmv ASC)  AS lr
-    FROM f)
+           ROW_NUMBER() OVER(PARTITION BY channel ORDER BY gmv ASC)  AS lr FROM f)
 SELECT channel,
        MAX(CASE WHEN br=1 THEN category END) AS best_selling_category,
-       MAX(CASE WHEN lr=1 THEN category END) AS least_selling_category
-FROM r
+       MAX(CASE WHEN lr=1 THEN category END) AS least_selling_category FROM r
 GROUP BY channel;
 
 -- 2. GMV Comparison
@@ -18,7 +16,6 @@ SELECT category,
        MAX(CASE WHEN channel='MT' THEN gmv END) AS mt_gmv,
        MAX(CASE WHEN channel='KA' THEN gmv END) AS ka_gmv,
        MAX(CASE WHEN channel='SME' THEN gmv END) AS sme_gmv
-FROM sales_data
-WHERE gmv >= 1 AND NOT (category = 'Non-veggie' AND gmv > 70)
+FROM sales_data WHERE gmv >= 1 AND NOT (category = 'Non-veggie' AND gmv > 70)
 GROUP BY category
 ORDER BY category;
